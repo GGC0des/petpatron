3b4df4f7c2df5a4380dcfb06888aac1f8a21d9db
Georgina Spieth <gjspieth@gmail.com>
2022-11-30 12:02:46 +0100

    added description migration to shelters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_160525) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_110132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,6 +94,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_160525) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_shelters_on_user_id"
   end
 
