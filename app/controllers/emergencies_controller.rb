a314b7362021bf41376ab256e0d1acbed5be7bda
Georgina Spieth <gjspieth@gmail.com>
2022-12-01 10:17:18 +0100

    emergencies controller for index show works

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -1,2 +1,17 @@
 class EmergenciesController < ApplicationController
+
+  def index
+    @emergencies = Emergency.all
+  end
+
+  def show
+    @emergency = Emergency.find(params[:id])
+    @donation = Donation.new
+  end
+
+  private
+
+  def emergency_params
+    params.require(:emergency).permit(:title, :description, :fundraising_goal, photos: [])
+  end
 end
