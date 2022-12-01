c2c44fe677d11830abf16e96cd15b14861eed587
Georgina Spieth <gjspieth@gmail.com>
2022-12-01 15:01:58 +0100

    emergencies controller modified, dashboard modified

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -11,6 +11,50 @@ class EmergenciesController < ApplicationController
     @donation = Donation.new
   end
 
+  def new
+    if user_signed_in? && current_user.shelter.present?
+      @emergency = Emergency.new
+    else
+      flash[:warning] = "Sorry, you are not a shelter owner"
+      redirect_to dashboard_path
+    end
+  end
+
+  def create
+    if user_signed_in?
+      @animal = Animal.new(animal_params)
+      @animal.shelter = current_user.shelter
+      if @animal.save
+        redirect_to dashboard_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def edit
+    if user_signed_in? && current_user.shelter.present?
+      @animal = Animal.find(params[:id])
+    else
+      flash[:warning] = "Sorry, you are not a shelter owner"
+    end
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      redirect_to animal_path(@animal)
+    else
+      render :update, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    animal = Animal.find(params[:id])
+    animal.destroy
+    redirect_to dashboard_path
+  end
+
   private
 
   def emergency_params
