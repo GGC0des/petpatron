cbe34544bd8a4529f9121636df7ccc016d395ad9
Niklas Held <niklas.held@gmail.com>
2022-12-02 16:08:07 +0100

    work

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,7 +1,25 @@
 class DonationsController < ApplicationController
 
   def new
+    @emergency = Emergency.find(params[:emergency_id])
     @donation = Donation.new
   end
 
+  def create
+    @emergency = Emergency.find(params[:emergency_id])
+    @donation = Donation.new(donation_params)
+    @donation.emergency = @emergency
+    @donation.user = current_user if user_signed_in?
+    if @donation.save!
+      redirect_to emergency_path(@emergency)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def donation_params
+    params.require(:donation).permit(:comment, :donation_amount)
+  end
 end
