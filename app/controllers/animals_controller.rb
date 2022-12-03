c79abcc2ed9735dabb2de041dce5a131673c145d
Amani <amani.maklad.bkr@gmail.com>
2022-12-03 23:58:11 +0100

    redirect when log in to the page you where at

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -20,7 +20,6 @@ class AnimalsController < ApplicationController
   def show
     @animal = Animal.find(params[:id])
     @caretaking = Caretaking.new
-    store_location_for(:user, animal_path(params[:id]))
     @shelters = Shelter.all
     @markers = @shelters.geocoded.map do |shelter|
       {
