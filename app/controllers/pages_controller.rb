ccaff4b017ead1de4164f900789100b4f5dc27bc
Georgina Spieth <gjspieth@gmail.com>
2022-12-05 12:38:57 +0100

    Default avatar added to comments in donations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,8 +9,8 @@ class PagesController < ApplicationController
     @myshelter = current_user.shelter # equal to Shelter.where(user: current_user).first
     @shelter_animals = Animal.where(shelter: current_user.shelter)
     @shelter_caretakings = Caretaking.where(animal: @shelter_animals) # bookings the shelter has received
-    @confirmed_shelter_caretakings = Caretaking.where(animal: @shelter_animals, status: 1) # bookings the shelter has received
-    @pending_shelter_caretakings = Caretaking.where(animal: @shelter_animals, status: 0) # bookings the shelter has received
+    @confirmed_shelter_caretakings = Caretaking.where(animal: @shelter_animals, status: 1) # confirmed bookings by shelter
+    @pending_shelter_caretakings = Caretaking.where(animal: @shelter_animals, status: 0) # pending bookings to be confirmed by shelter
     @user_caretakings = Caretaking.where(user: current_user) # what the user has booked
     @shelter_emergencies = Emergency.where(animal: @shelter_animals)
     @shelter_donations = Donation.where(emergencies: @shelter_emergencies)
