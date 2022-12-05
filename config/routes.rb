7326ead0cfee98852999775f0d115d318b2bfbc2
Georgina Spieth <gjspieth@gmail.com>
2022-12-05 11:19:10 +0100

    Confirm path added

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,12 @@ Rails.application.routes.draw do
   resources :shelters
 
   resources :animals do
-    resources :caretakings, only: [:create, :show]
-    resources :emergencies, only: [:new, :create]
+    resources :caretakings, only: [:create, :show, :destroy]
+    resources :emergencies, only: [:new, :create, :destroy]
   end
 
   resources :emergencies, only: [:index, :show, :destroy] do
     resources :donations, only: [:create, :show]
   end
+  put "/caretakings/:id/confirm", to: "caretakings#confirm!", as: 'confirm_caretaking'
 end
