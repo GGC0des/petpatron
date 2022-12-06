10bd70c75a3a2fcad2deb25582d4694fd31b0bd6
Georgina Spieth <gjspieth@gmail.com>
2022-12-06 12:14:50 +0100

    Dashboard functionality works

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   resources :caretakings, only: [:destroy]
   put "/caretakings/:id/confirm", to: "caretakings#confirm!", as: 'confirm_caretaking'
 
-  resources :emergencies, only: [:index, :show, :destroy] do
+  resources :emergencies, only: [:index, :show, :edit, :update, :destroy] do
     resources :donations, only: [:new, :create, :show]
   end
 end
