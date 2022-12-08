53c31111e19058c38c2493cf022e1e62ea830932
Amani <amani.maklad.bkr@gmail.com>
2022-12-09 00:18:01 +0100

    fixing error happen when selecting category and searching with words having typos

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -9,6 +9,11 @@ class AnimalsController < ApplicationController
         OR categories.name ILIKE :query
         OR animals.name ILIKE :query
       "
+      # fixing error happen when selecting category and searching with words having typos
+      if params[:categories]
+        @categories = Category.where(name: params[:categories]).pluck(:id).join(",")
+        sql_query = "(#{sql_query}) AND categories.id IN (#{@categories})"
+      end
       # to get results from searching by name
       @items = Animal.joins(:shelter, :categories).where(sql_query, query: "%#{params[:query]}%").distinct
       # .distinct to avoid repeated result
@@ -26,10 +31,6 @@ class AnimalsController < ApplicationController
       @animals = Animal.all
     end
 
-    if params[:categories]
-      @categories = Category.where(name: params[:categories])
-      @animals = @animals.joins(animal_categories: :category).where(category: {id: @categories})
-    end
     @markers = @shelters.map do |shelter|
       {
         lat: shelter.latitude,
