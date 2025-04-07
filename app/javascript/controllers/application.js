9c30d780ed38e49f5d1b9802115f4c2374436aff
Georgina Spieth <gjspieth@gmail.com>
2025-04-07 18:52:38 +0200

    bootstrap exposed globally, disclaimer_modal_controller bugfix

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,13 +1,10 @@
+// for initializing Stimulus only
 import { Application } from "@hotwired/stimulus"
-import { Modal } from 'bootstrap'
-
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-
-import "controllers"
 
 export { application }
