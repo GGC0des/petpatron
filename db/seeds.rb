b92ca85b1061b9fe95ddd719c1f8114e67a2baa8
Georgina Spieth <gjspieth@gmail.com>
2025-04-10 11:59:22 +0200

    Fixed seed path for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 # split local and production seed files in order to safely hide the seedfile from Github rather than .gitignore
 if Rails.env.production?
-  load(Rails.root.join('db', 'seeds.heroku.rb'))
+  path = Rails.root.join('db', 'seeds.heroku.rb')
+  load(path) if File.exist?(path)
 else
   load(Rails.root.join('db', 'seeds.local.rb'))
 end
